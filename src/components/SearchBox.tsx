1848665e5ddcec030ad535c57030b0c44cf019bb
Perla Aguilar <perlaelizabeth_am@hotmail.com>
2023-12-03 12:41:39 -0600

    Search component

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import logo from '../assets/Logo_ML.png';
+import logo2x from '../assets/Logo_ML@2x.png.png';
+import searchIcon from '../assets/ic_Search.png';
+import searchIcon2x from '../assets/ic_Search@2x.png.png';
+
+export default function SearchBox(props: any) {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <header className='container-search-box'>
+      <div className='box'>
+        <img src={logo} srcSet={`${logo2x} 2x`} alt='Logo Mercado Libre' />
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='Nunca dejes de buscar'
+            aria-label='Ingresa el texto del producto que deseas buscar'
+            value={searchValue}
+            onChange={handleChange}
+          />
+          <button type='submit' aria-label='Buscar producto'>
+            {' '}
+            <img
+              className='icon-search'
+              src={searchIcon}
+              srcSet={`${searchIcon2x} 2x`}
+              alt='Buscar'
+            />
+          </button>
+        </form>
+      </div>
+    </header>
+  );
+}
